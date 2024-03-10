a2818df6250cd58cc32c182ad133706ab7a18595
carlosarguelles <carlosjarguelles@icloud.com>
2024-03-10 10:16:50 -0500

    update fish

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,25 +1,11 @@
-alias v "nvim"
-alias vconf "cd ~/.config/nvim && v"
-
-alias yta "youtube-dl --extract-audio --audio-format mp3 --output '~/Downloads/%(title)s.%(ext)s'" 
-
-alias yabairc "v ~/.config/yabai/yabairc"
-alias skhdrc "v ~/.config/skhd/skhdrc"
-
+alias v nvim
+alias vconf "cd ~/.dotfiles/nvim/.config/nvim && v"
 alias b "brew install"
 alias bc "brew install --cask"
 alias bs "brew search"
-
 alias sub "osdb get -l spa"
-
-function config
-    command git --git-dir=$HOME/dotfiles --work-tree=$HOME $argv
-end
-
 alias lds "diskutil list"
-
-alias l "exa -l -g --icons --group-directories-first"
+alias l "eza -l -g --icons --group-directories-first"
 alias ll "l -a"
-alias cl "clear"
-
+alias cl clear
 alias kssh "kitty +kitten ssh"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,39 +1,38 @@
 #!/usr/local/bin/fish
-
 fish_vi_key_bindings
 set -U fish_cursor_default block
 set fish_greeting ""
-
 set -gx EDITOR nvim
-
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
-
+set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH /usr/local/bin $PATH
 set -gx PATH /usr/local/sbin $PATH
-
-set -gx LOCAL /opt/local  
+set -gx LOCAL /opt/local
 set -gx PATH $LOCAL/bin $PATH
 set -gx PATH $LOCAL/sbin $PATH
-
 set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-
-set -gx PATH /Library/TeX/texbin/ $PATH
-
+set -gx PATH $HOME/Library/TinyTeX/bin/universal-darwin/ $PATH
 set -gx PATH /usr/local/go/bin/ $PATH
 set -gx GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH 
-
+set -gx PATH $GOPATH/bin $PATH
+set -gx PATH $HOME/Library/Python/3.9/bin $PATH
 set -gx PATH $HOME/.cargo/bin $PATH
-set -gx PATH $HOME/.composer/vendor/bin $PATH
-
-set -gx PATH /usr/local/opt/openjdk/bin $PATH
-set -gx CPPFLAGS "-I/usr/local/opt/openjdk/include"
-set -gx JAVA_HOME /usr/local/Cellar/openjdk/16.0.2/libexec/openjdk.jdk/Contents/Home
-
+set -gx BUN_INSTALL $HOME/.bun
+set -gx PATH $BUN_INSTALL/bin $PATH
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/colors.fish
+source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
 
-# starship init fish | source
+# opam configuration
+source /Users/carlos/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
+
+# pnpm
+set -gx PNPM_HOME /Users/carlos/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
