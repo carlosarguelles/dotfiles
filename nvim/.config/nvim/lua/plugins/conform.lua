717bc1e522a1c30611acec7ab465cf633cf365b8
carlosarguelles <carlosjarguelles@icloud.com>
2024-03-11 12:26:04 -0500

    Use biome as ft for esm type lang when `biome.json` is present in root

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,26 @@
+local js_ft = function(bufnr)
+  local uri = vim.fs.find({ "biome.json" }, {
+    upward = true,
+    type = "file",
+    stop = vim.loop.os_homedir(),
+    path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
+  })[1]
+  if uri == nil or uri == "" then
+    return { "prettierd" }
+  else
+    return { "biome" }
+  end
+end
+
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { { "prettierd", "biome" } },
-      typescript = { { "prettierd", "biome" } },
-      javascriptreact = { { "prettierd", "biome" } },
-      typescriptreact = { { "prettierd", "biome" } },
-      json = { { "prettierd", "biome" } },
+      javascript = js_ft,
+      typescript = js_ft,
+      javascriptreact = js_ft,
+      typescriptreact = js_ft,
+      json = js_ft,
       java = { "google-java-format" },
       templ = { "templ" },
       ocaml = { "ocamlformat" },
