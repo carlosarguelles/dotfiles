a84d61b375b435914a57f9c69403e2a16cf6e671
carlosarguelles <carlosjarguelles@icloud.com>
2024-04-16 09:43:35 -0500

    improve keymaps of oil.nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,14 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    keymaps = {
+      ["<C-v>"] = "actions.select_vsplit",
+      ["<C-x>"] = "actions.select_split",
+      ["<C-p>"] = false,
+      ["<C-l>"] = false,
+      ["<C-h>"] = false,
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "<C-e>", "<cmd>Oil<cr>", desc = "Toggle Oil" },
